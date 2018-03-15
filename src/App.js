715c74a9bbc5dc74d55977c2d3529f67b26bc58c
HaiYen <Fiststar1995@gmail.com>
2018-03-15 16:39:45 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class A extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Nhung</h1>
+          <h1 className="text-warring">Welcome to Nhung</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default A;
