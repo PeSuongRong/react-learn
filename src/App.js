d8471266661d3cb4ca76ad9c6e593d6b3797d326
HaiYen <Fiststar1995@gmail.com>
2018-03-16 18:07:20 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 ﻿import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Product from './components/Product';
 
-class A extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="text-warring">Welcome to Nhung</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <div className=" container">
+          <div className="row">
+            <Product />
+            <Product />
+            <Ifo />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default A;
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Demo from './cat-product/Demo';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Demo />, document.getElementById('yen'));
 registerServiceWorker();
